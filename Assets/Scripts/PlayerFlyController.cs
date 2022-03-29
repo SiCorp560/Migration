05798ddb5cc3bed2be2f48aeb0490af4bbedacd5
hcarter20 <hcarter@andrew.cmu.edu>
2022-03-29 17:35:36 -0400

    Updated player movement, wind implementation

--- Assets/Scripts/PlayerFlyController.cs
+++ Assets/Scripts/PlayerFlyController.cs
@@ -33,20 +33,25 @@ public class PlayerFlyController : MonoBehaviour
     private readonly int IDLE = 0, WALK = 1, FLY = 2, FLAP = 3, FALL = 4;
 
     // Used to track player's movement state during gameplay
-    private bool flying = false;
-    private bool onGround = true;
+    private bool flying = true;
+    private bool onGround = false;
+
+    // Used to track when player is affected by wind zone
+    private bool inWind = false;
+    private Vector2 windEffect = Vector2.zero;
     
     // Used to track the player's stamina throughout the level
-    public int defaultStamina;
-    public int slowdownTime;
-    public int maxStamina;  // Made public in order to manip thru editor
+    public int defaultStamina; // maxStamina before collecting any flowers
+    public int slowdownTime;   // How much time flying after stamina runs out
+    public int maxStamina;     // total stamina, upgraded with collectibles
+    private float timeLeft;
 
     // Used to track the player's flapping to create gusts
     private bool flapping = false;
     public int gustPower = 1;
 
     // Used to track the coroutine which controls the player's flight stamina
-    private Coroutine flyingCoroutine = null;
+    // private Coroutine flyingCoroutine = null;
 
     // Used to mark the stunned state after hit by water droplet
     private bool stunned = false;
@@ -83,7 +88,8 @@ public class PlayerFlyController : MonoBehaviour
         // Set the state to flying
         onGround = false;
         flying = true;
-        flyingCoroutine = StartCoroutine(FlyStamina());
+        timeLeft = maxStamina + slowdownTime;
+        // flyingCoroutine = StartCoroutine(FlyStamina());
     }
 
     private void StopFlying()
@@ -93,7 +99,7 @@ public class PlayerFlyController : MonoBehaviour
 
         // Disable the flying variables
         flying = false;
-        StopCoroutine(flyingCoroutine);
+        // StopCoroutine(flyingCoroutine);
 
         // Cancel any flapping (can't gust while out of stamina)
         if (flapping)
@@ -134,6 +140,19 @@ public class PlayerFlyController : MonoBehaviour
         //gustObject.SetActive(false);
     }
 
+    /* Used by Wind Zone to tell butterfly when it's being pushed */
+    public void StartWind(Vector2 windVector)
+    {
+        inWind = true;
+        windEffect = windVector;
+    }
+
+    public void StopWind()
+    {
+        inWind = false;
+        windEffect = Vector2.zero;
+    }
+
     private void Update()
     {
         if (onGround && !stunned)
@@ -159,50 +178,99 @@ public class PlayerFlyController : MonoBehaviour
         else if (flapping && Input.GetButtonUp("Flap"))
             StopFlapping();
 
-        // Player can't move while flapping
-        if (!flapping)
+        // Is the player doint something that requires stamina?
+        bool usingStamina = false;
+
+        // Player can't move (under own power) while flapping
+        if (flapping)
+        {
+            usingStamina = true;
+
+            // Player still gets pushed by the wind
+            if (inWind)
+            {
+                rb.velocity = windEffect;
+            }
+        }
+        else
         {
             // Check for horizontal & vertical movement input
             float xMove = Input.GetAxisRaw("Horizontal");
             float yMove = Input.GetAxisRaw("Vertical");
 
-            // Halve the player's movement when stunned
+            // Determine the max downward velocity
+            float fallingSpeed = glidingSpeed;
+
+            // Halve the player's movement when stunned (wet), fall faster
             if (stunned)
             {
                 xMove /= 2;
-                yMove /= 2; // Shouldn't matter?
+                yMove /= 2;
+                fallingSpeed += 2.0f;
             }
 
             // Movement depends on the current state of the butterfly
             if (flying)
             {
+                if (xMove != 0.0f || yMove == 1.0f)
+                {
+                    // Uses stamina when flying horizontally or up
+                    usingStamina = true;
+                    animator.SetInteger("state", FLY);
+                }
+                else if (yMove == -1.0f)
+                {
+                    // If only moving down, don't use stamina, just glide
+                    animator.SetInteger("state", FALL);
+                }
+
                 // Make sure moving diagonally doesn't give you extra speed
                 Vector2 moveVector = new Vector2(xMove, yMove);
 
                 // Directly update the player's velocity
                 rb.velocity = moveVector.normalized * moveSpeed;
 
-                // Make sure wind effect doesn't accelerate too much
-                if (rb.velocity.y < 0 && Mathf.Abs(rb.velocity.y) > glidingSpeed)
-                    rb.velocity = new Vector2(rb.velocity.x, -glidingSpeed);
+                // Add any wind effects
+                if (inWind)
+                    rb.velocity += windEffect;
 
-                // Update to the flying animation
-                animator.SetInteger("state", FLY);
+                // Make sure wind effect doesn't accelerate too much
+                if (rb.velocity.y < 0 && Mathf.Abs(rb.velocity.y) > fallingSpeed)
+                    rb.velocity = new Vector2(rb.velocity.x, -fallingSpeed);
+            }
+            else if (!onGround)
+            {
+                // Allow vertical movement, but only down, and apply gravity
+                float yVelocity = Mathf.Min(rb.velocity.y, 0.0f);
+                yVelocity += -9.8f * Time.deltaTime;
+                float newY = -Mathf.Min(Mathf.Abs(yVelocity), fallingSpeed);
+
+                // Directly update the player's velocity
+                rb.velocity = new Vector2(xMove * walkingSpeed, newY);
+
+                // Add wind effects
+                if (inWind)
+                    rb.velocity += windEffect;
+
+                // Update animation
+                animator.SetInteger("state", FALL);
             }
             else
             {
                 // Manually apply "gravity" up to terminal velocity cap
                 float yVelocity = rb.velocity.y;
                 yVelocity += -9.8f * Time.deltaTime;
-                float newY = -Mathf.Min(Mathf.Abs(yVelocity), glidingSpeed);
+                float newY = -Mathf.Min(Mathf.Abs(yVelocity), fallingSpeed);
 
                 // Directly update the player's velocity, horizontally
                 rb.velocity = new Vector2(xMove * walkingSpeed, newY);
 
+                // Add wind effects at half strength when grounded
+                if (inWind)
+                    rb.velocity += (windEffect / 2.0f);
+
                 // Update animation based on movement
-                if (!onGround)
-                    animator.SetInteger("state", FALL);
-                else if (xMove == 0.0f)
+                if (xMove == 0.0f)
                     animator.SetInteger("state", IDLE);
                 else
                     animator.SetInteger("state", WALK);
@@ -220,6 +288,10 @@ public class PlayerFlyController : MonoBehaviour
                 transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
             }
         }
+
+        // Only count down stamina timer when using up stamina
+        if (usingStamina)
+            UpdateStaminaTimer();
     }
 
     // Triggered by the checkpoint, to prevent double activation
@@ -228,6 +300,30 @@ public class PlayerFlyController : MonoBehaviour
         maxStamina += 1;
     }
 
+    private void UpdateStaminaTimer()
+    {
+        if (timeLeft > slowdownTime)
+        {
+            // Time with full speed flying
+            timeLeft -= Time.deltaTime;
+        }
+        else if (timeLeft > 0.0f)
+        {
+            // Slowed down flying, time left until run out of stamina
+            timeLeft -= Time.deltaTime;
+
+            // Update the player's animation
+            float animSpeed = Mathf.Lerp(0.1f, 0.5f, (float)timeLeft / (float)slowdownTime);
+            animator.SetFloat("flapSpeed", animSpeed);
+        }
+        else
+        {
+            // When timer runs out, cancel flying ability
+            StopFlying();
+        }
+    }
+
+    /*
     private IEnumerator FlyStamina()
     {
         int timeLeft = maxStamina;
@@ -261,6 +357,7 @@ public class PlayerFlyController : MonoBehaviour
         // When timer runs out, cancel flying ability
         StopFlying();
     }
+    */
 
     public void KnockDown()
     {
--- Assets/Scripts/WindEffect.cs
+++ Assets/Scripts/WindEffect.cs
@@ -5,49 +5,44 @@ using UnityEngine;
 public class WindEffect : MonoBehaviour
 {
     // The direction that the wind should be applied in
-    public Vector3 direction = Vector3.left;
+    public Vector2 direction = Vector2.left;
     // The force of the wind
-    public int windForce = 3;
-    // Max speed of player when being entirely pushed by wind
-    public int maxWindSpeed = 5;
+    public float windForce = 3;
 
     // Whether player is currently within zone and should be affected
     private bool inWind = false;
 
-    void FixedUpdate()
+  
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        if (inWind)
+        if (collision.gameObject.CompareTag("Player") && !inWind)
         {
-            // For convenience, shorter name
-            PlayerFlyController player = PlayerFlyController.player;
-            if (player != null)
+            // Activate this wind effect
+            inWind = true;
+
+            if (PlayerFlyController.player != null)
             {
                 // The acceleration force to apply to the player
                 Vector2 windVector = direction * windForce;
 
-                // Apply the force directly to the player's rigidbody
-                player.rb.velocity += windVector * Time.fixedDeltaTime;
-
-                // Cap the player's speed to prevent too much acceleration
-
-                // TODO: Different versions for when player is flying with wind vs against wind?
+                // Tell the player controller to apply wind force
+                PlayerFlyController.player.StartWind(windVector);
             }
-        }
-    }
-  
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            inWind = true;
         }
     }
   
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && inWind)
         {
+            // De-activate this wind effect
             inWind = false;
+
+            if (PlayerFlyController.player != null)
+            {
+                // Tell the player controller to stop applying wind force
+                PlayerFlyController.player.StopWind();
+            }
         }
     }
 }
